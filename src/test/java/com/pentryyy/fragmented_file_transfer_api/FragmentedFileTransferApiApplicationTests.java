53102f72f1c5355b4dff921a282f66c31a41e30b
pentryyy <m.v.volikov@gmail.com>
2025-06-13 18:51:14 +0300

    Init

---
+++ src/test/java/com/pentryyy/fragmented_file_transfer_api/FragmentedFileTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentryyy.fragmented_file_transfer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentedFileTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
